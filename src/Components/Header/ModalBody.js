7a7c2194d478679ff27335a567052383c8223b0a
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-11-26 22:12:00 +0000

    wire up modal close

--- src/Components/Header/ModalBody.js
+++ src/Components/Header/ModalBody.js
@@ -1,11 +1,22 @@
+import { AiOutlineCloseCircle } from 'react-icons/ai'
+
+import './ModalBody.css'
+
 const ModalBody = (props) => {
   const {
     closeModal
   } = props;
 
   return (
-    <div>
-      Modal body
+    <div className='modalbody-wrapper'>
+      <div className='modalbody-header'>
+        <AiOutlineCloseCircle
+          onClick={closeModal}
+        />
+      </div>
+      <div className='modalbody-body'>
+        Body goes here
+      </div>
     </div>
   )
 }
