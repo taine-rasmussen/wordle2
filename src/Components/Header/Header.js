ec1d02fc7708776618fe1de77bae1c718692cf4e
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-11-26 21:55:50 +0000

    open/close funcs for modal

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,22 @@
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 import { BsBarChart } from 'react-icons/bs';
+import Modal from '@material-ui/core/Modal';
+import { useState } from 'react';
 
 import './Header.css';
 
 const Header = () => {
+
+  const [toggleModal, setToggleModal] = useState(false);
+
+  const openModal = () => {
+    setToggleModal(true)
+  };
+
+  const closeModal = () => {
+    setToggleModal(false)
+  };
+
   return (
     <div className='header-wrapper'>
       <h3>Wordle</h3>
