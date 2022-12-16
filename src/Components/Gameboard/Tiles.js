8dfeffca494d3127656caa94fc43b1e004d63398
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-12-16 20:46:10 +0000

    map out tiles onto gameboard:

--- src/Components/Gameboard/Tiles.js
+++ src/Components/Gameboard/Tiles.js
@@ -1,13 +1,32 @@
 import React from 'react'
+import useGameboardRows from '../../Hooks/useGameboardRows'
 import './Tiles.css'
 
 
 const Tiles = () => {
-  return (
-    <div className='tiles-container'>
-      
-    </div>
-  )
+
+  const {
+    gameboardRows
+  } = useGameboardRows()
+
+return(
+  <div className='tiles-container'>
+    {gameboardRows.map((row, i) => {
+      return(
+        <div className='row-index'>
+          {row.map((tile, j) => {
+            return(
+              <div
+                className='tile-cell'
+              >
+                x
+              </div>
+            )
+          })} 
+        </div>
+      )})}
+  </div>
+)
 }
 
 export default Tiles
--- src/Hooks/useGameboardRows.js
+++ src/Hooks/useGameboardRows.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const useGameboardRows = () => {
 
-  const [gameboardRows, setGameboardRows] = ([
+  const [gameboardRows, setGameboardRows] = useState([
     [
       { key: '', style: { backgroundColor: '' }, found: false },
       { key: '', style: { backgroundColor: '' }, found: false },
